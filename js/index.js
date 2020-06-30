6234b9bff0da4a84de029d12624868c4fe1a3a43
xy <1277493047@qq.com>
2020-06-30 22:01:32 +0800

    1.0.3

--- js/index.js
+++ js/index.js
@@ -1,10 +1,149 @@
 $(function () {
+  //banner轮播图
   $.get(
     "../server/lunbo1.php",
     (data) => {
-      console.log(data);
-      lunbo("#banner-wrap", data, 1000, 90, 28);
+      lunbo("#banner-wrap", data, 1500, 90, 28, 1);
     },
     "json"
   );
+  //心选tab
+  $($(".xinxuan-tab").children()[0])
+    .children("li")
+    .on("click", "span", function () {
+      $(this)
+        .parent()
+        .siblings()
+        .children("span")
+        .css({ borderBottom: "none", color: "#000" });
+      $(this).css({
+        color: "#005aaa",
+        borderBottom: "0.3rem solid #005aaa",
+      });
+    });
+  //心选大图鼠标滑过
+  $(".xinxuan-content-wrap").hover(
+    function () {
+      $(".xinxuan-content-wrap").css({
+        borderBottom: "0.4rem solid #005aaa",
+        boxShadow: "0 0.5rem 2rem gray",
+        zIndex: "1000",
+      });
+      $(".xinxuan-content-wrap").children("img").css({
+        display: "block",
+        width: "25rem",
+        height: "25rem",
+        margin: "0",
+        transition: "0.2s linear",
+      });
+    },
+    function () {
+      $(".xinxuan-content-wrap").css({
+        borderBottom: "none",
+        boxShadow: "none",
+        zIndex: "1",
+      });
+      $(".xinxuan-content-wrap").children("img").css({
+        width: "23rem",
+        height: "23rem",
+
+        margin: "1rem",
+      });
+    }
+  );
+  //心选小图鼠标划过
+  $(".xxcs").hover(
+    function () {
+      $(this).css({
+        borderBottom: "0.3rem solid #005aaa",
+        zIndex: "100",
+        boxShadow: "0 0.5rem 1rem gray",
+      });
+      $(this).children("img").css({
+        width: "11rem",
+        height: "11rem",
+        margin: "1rem auto 0",
+        transition: "0.3s linear",
+      });
+    },
+    function () {
+      $(this).css({
+        borderBottom: "none",
+        boxShadow: "none",
+        zIndex: "1",
+      });
+      $(this).children("img").css({
+        width: "8rem",
+        height: "8rem",
+        margin: "3rem auto 0",
+      });
+    }
+  );
+  //人气tab
+  $($(".renqi-tab").children()[0])
+    .children("li")
+    .on("click", "span", function () {
+      $(this)
+        .parent()
+        .siblings()
+        .children("span")
+        .css({ borderBottom: "none", color: "#000" });
+      $(this).css({
+        color: "#005aaa",
+        borderBottom: "0.3rem solid #005aaa",
+      });
+    });
+  //人气show
+  $(".renqi-show-box").hover(
+    function () {
+      $(this).children("img").css({
+        width: "19rem",
+        height: "19rem",
+        margin: "0",
+        transition: "0.2s linear",
+      });
+      $(this).css({
+        boxShadow: "0 0.5rem 1rem gray",
+      });
+    },
+    function () {
+      $(this).children("img").css({
+        width: "17rem",
+        height: "17rem",
+        margin: "1rem",
+      });
+      $(this).css({
+        boxShadow: "0 0.25rem 0.5rem gray",
+      });
+    }
+  );
+  //活动板块
+  $(".hd-img").hover(
+    function () {
+      $(this).css({
+        borderBottom: "0.3rem solid #005aaa",
+        boxShadow: "0 0.5rem 1rem gray",
+        zIndex: "3",
+      });
+      $(this).children("img").css({
+        width: "120%",
+        height: "120%",
+        margin: "-2rem 0 0 -2rem",
+        transition: "0.2s linear",
+      });
+    },
+    function () {
+      $(this).css({
+        borderBottom: "none",
+        boxShadow: "none",
+        zIndex: "1",
+      });
+      $(this).children("img").css({
+        width: "100%",
+        height: "100%",
+        margin: "0",
+      });
+    }
+  );
+  //监听底部
 });
--- js/lunbotu.js
+++ js/lunbotu.js
@@ -1,5 +1,4 @@
-function lunbo(oDiv, data, sudu, kuan, gao,doudou) {
-  this.kuan = kuan;
+function lunbo(oDiv, data, sudu, kuan, gao, doudou) {
   let html = "";
   let oUl = $(`<ul ></ul>`);
   let lSpan = $(
@@ -8,10 +7,38 @@ function lunbo(oDiv, data, sudu, kuan, gao,doudou) {
   let rSpan = $(
     "<span style='border-radius: 17.5rem;width: 3.5rem;height: 3.5rem;'>&gt;</span>"
   );
- let dd  = $("")
+  let dd = $("<figure></figure>");
+  //豆豆
+  dd.css({
+    position: "absolute",
+    width: `${data.length * 3}rem`,
+    bottom: "2rem",
+    left: `${kuan / 2}rem`,
+    marginLeft: `-${data.length * 1.5}rem`,
+    zIndex: "999",
+    color: "white",
+  });
+  dd.appendTo($(`${oDiv}`));
+  let html1 = "";
+  for (let k = 0; k < data.length; k++) {
+    html1 += '<i class="iconfont">&#xe6c3;</i>';
+  }
+  dd.html(html1);
 
+  $(dd[0].querySelectorAll("i")).css({
+    fontSize: "0.5rem",
+    width: "2rem",
+    lineHeight: "2rem",
+    float: "left",
+    margin: "0 0.1rem",
+    height: "2rem",
+    textAlign: "center",
+    borderRadius: "2rem",
+  });
+  $(dd[0].querySelectorAll("i")[0]).css("border", "0.1rem solid white");
+  //控制键
   lSpan.css({
-    display: "block",
+    display: "none",
     background: "white",
     position: "absolute",
     top: `${gao / 2}rem`,
@@ -22,18 +49,21 @@ function lunbo(oDiv, data, sudu, kuan, gao,doudou) {
     fontSize: "2rem",
     color: "#000",
   });
+  //向左移动
   lSpan.on("click", () => {
-    i++;
+    i--;
     if (i > data.length - 1) {
       i = 0;
     } else if (i < 0) {
       i = data.length - 1;
     }
     console.log(i);
-    oUl.css("left", `-${this.kuan * i}rem`);
+    oUl.css("left", `-${kuan * i}rem`);
+    dd.children().css("border", "none");
+    $(dd.children()[i]).css("border", "0.1rem solid white");
   });
   rSpan.css({
-    display: "block",
+    display: "none",
     background: "white",
     position: "absolute",
     top: `${gao / 2}rem`,
@@ -44,18 +74,22 @@ function lunbo(oDiv, data, sudu, kuan, gao,doudou) {
     fontSize: "2rem",
     color: "#000",
   });
+  //向右移动
   rSpan.on("click", () => {
-    i--;
+    i++;
     if (i > data.length - 1) {
       i = 0;
     } else if (i < 0) {
       i = data.length - 1;
     }
     console.log(i);
-    oUl.css("left", `-${this.kuan * i}rem`);
+    dd.children().css("border", "none");
+    $(dd.children()[i]).css("border", "0.1rem solid white");
+    oUl.css("left", `-${kuan * i}rem`);
   });
   lSpan.appendTo($(`${oDiv}`));
   rSpan.appendTo($(`${oDiv}`));
+  //轮播图
   oUl.css({
     width: `${data.length * kuan}rem`,
     height: `${gao}rem`,
@@ -75,6 +109,7 @@ function lunbo(oDiv, data, sudu, kuan, gao,doudou) {
     height: `${gao}rem`,
   });
   let i = 0;
+  //轮播逻辑
   let myTimer = setInterval(() => {
     i++;
     if (i > data.length - 1) {
@@ -82,15 +117,19 @@ function lunbo(oDiv, data, sudu, kuan, gao,doudou) {
     } else if (i < 0) {
       i = data.length - 1;
     }
-
-    oUl.css("left", `-${this.kuan * i}rem`);
-
-    console.log(myTimer);
+    $(dd.children()).css("border", "none");
+    $(dd.children()[i]).css({
+      border: "0.1rem solid white",
+    });
+    oUl.css("left", `-${kuan * i}rem`);
   }, sudu);
+  //banner逻辑
   $(oDiv).on("mouseenter", function () {
     clearInterval(myTimer);
+    $(oDiv).children("span").css("display", "block");
   });
   $(oDiv).on("mouseleave", () => {
+    $(oDiv).children("span").css("display", "none");
     myTimer = setInterval(() => {
       i++;
       if (i > data.length - 1) {
@@ -98,7 +137,21 @@ function lunbo(oDiv, data, sudu, kuan, gao,doudou) {
       } else if (i < 0) {
         i = data.length - 1;
       }
-      oUl.css("left", `-${this.kuan * i}rem`);
+      $(dd.children()).css("border", "none");
+      $(dd.children()[i]).css({
+        border: "0.1rem solid white",
+      });
+      oUl.css("left", `-${kuan * i}rem`);
     }, sudu);
   });
+  //豆豆逻辑
+  $(dd).on("click", "i", function () {
+    index = $(this).index();
+    dd.children().css("border", "none");
+    $(dd.children()[index]).css("border", "0.1rem solid white");
+    oUl.css("left", `-${index * kuan}rem`);
+  });
+  if (!doudou || doudou == undefined) {
+    dd.css("display", "none");
+  }
 }
